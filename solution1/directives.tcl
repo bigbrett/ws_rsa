3f8c38e2e81ee1eb241ae751c9d52320b41a917c
Brett <brett.r.nicholas@gmail.com>
2017-08-10 11:12:34 -0400

    added directives and scripts

---
+++ solution1/directives.tcl
@@ -0,0 +1,12 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_allocation -limit 1 -type function "rsaModExp" montMult
+set_directive_interface -mode s_axilite "rsaModExp" n
+set_directive_interface -mode s_axilite "rsaModExp" xbar
+set_directive_interface -mode s_axilite "rsaModExp" e
+set_directive_interface -mode s_axilite "rsaModExp" out
+set_directive_interface -mode s_axilite "rsaModExp" Mbar
+set_directive_interface -mode s_axilite "rsaModExp"
