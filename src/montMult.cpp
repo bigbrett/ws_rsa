aabafd049c6a2321f5dd6a8a9f256611edbc2fe5
Brett <brett.r.nicholas@gmail.com>
2017-08-10 20:26:48 -0400

    added correct r such that we can compute xbar and Mbar in testbench ;

--- src/montMult.cpp
+++ src/montMult.cpp
@@ -1,6 +1,5 @@
 #include "montMult.hpp"
 
-//void montMult(rsaSize_t X0, rsaSize_t Y0, rsaSize_t M0, rsaSize_t* outData)
 void montMult(ap_uint<NUM_BITS+2> X, ap_uint<NUM_BITS+2> Y, ap_uint<NUM_BITS+2> M, ap_uint<NUM_BITS+2>* outData)
 
 {
@@ -8,9 +7,6 @@ void montMult(ap_uint<NUM_BITS+2> X, ap_uint<NUM_BITS+2> Y, ap_uint<NUM_BITS+2> 
 	/* IMPT BUGFIX: INTERMEDIATE WIDTHS MUST BE GREATER
 	 * THAN OR EQUAL TO NUM_BITS+1 OR OVERFLOW CAUSES
 	 * THE INTEGER LIBRARY TO SCREW UP SIGN EXTENTIONS */
-//	ap_uint<NUM_BITS+2> S = 0;
-//	ap_uint<NUM_BITS+2> tmp,tmp1;
-//	ap_uint<NUM_BITS+2> X = X0, Y = Y0, M=M0;
 	ap_uint<NUM_BITS+2> S = 0;
 	ap_uint<NUM_BITS+2> tmp;
 	int i;
--- src/rsaModExp.cpp
+++ src/rsaModExp.cpp
@@ -2,12 +2,14 @@
 #include "montMult.hpp"
 
 
-void rsaModExp( ap_uint<NUM_BITS+2> Mbar, ap_uint<NUM_BITS+2> e, ap_uint<NUM_BITS+2> n,
-		ap_uint<NUM_BITS+2> xbar, ap_uint<NUM_BITS+2> *out)
+void rsaModExp( ap_uint<NUM_BITS+2> Mbar, // precomputed: montgomery residue of the base w.r.t. the radix--> (2^(2048+2))*base mod n
+				ap_uint<NUM_BITS+2> e,    // exponent
+				ap_uint<NUM_BITS+2> n,    // modulus
+				ap_uint<NUM_BITS+2> xbar, // precomputed: montgomery residue of 1  w.r.t. the radix--> 2^(2048+2) mod n
+				ap_uint<NUM_BITS+2> *out) // output
 {
 
 	// compute montgomery modular exponentiation using square and multiply algorithm
-//	for (i=NUM_BITS-1; i>=0; i--)
 	for (int i=NUM_BITS-1; i>=0; i--)
 	{
 		montMult(xbar,xbar,n,&xbar); // square
@@ -19,24 +21,3 @@ void rsaModExp( ap_uint<NUM_BITS+2> Mbar, ap_uint<NUM_BITS+2> e, ap_uint<NUM_BIT
 	montMult(xbar,(ap_uint<NUM_BITS+2>)1,n,out);
 }
 
-
-//void rsaModExp( uintRSA_t Mbar, uintRSA_t e, uintRSA_t n,
-//				uintRSA_t xbar, uintRSA_t *out)
-//{
-//	uintRSA_t esrl = e;
-//
-//	// compute montgomery modular exponentiation using square and multiply algorithm
-//	int i;
-//	for (i=NUM_BITS-1; i>=0; i--)
-//	{
-////		cout << "i = " << dec << i << endl;
-//		montMult(xbar,xbar,n,&xbar); // square
-////		cout << "| square: xbar = " << hex << xbar << endl;
-//		if (e.test(i)) {
-//			montMult(Mbar,xbar,n,&xbar); // multiply
-////			cout << "| multiply: xbar = " << xbar << endl;
-//		}
-//	}
-//	// undo montgomery transform
-//	montMult(xbar,(rsaSize_t)1,n,out);
-//}
